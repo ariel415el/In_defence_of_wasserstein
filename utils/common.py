c46b933c1e1a3ddd5d2f6226de08e37e7719a7b4
ariel <ariel415el@gmail.com>
2022-11-28 11:04:12 +0200

    Fixed loss error

--- utils/common.py
+++ utils/common.py
@@ -70,8 +70,8 @@ class SoftHingeLoss:
     def trainD(self, netD, real_data, fake_data):
         D_scores_real = netD(real_data)
         D_scores_fake = netD(fake_data.detach())
-        D_loss_real = F.relu(torch.rand_like(real_data) * 0.2 + 0.8 - D_scores_real).mean()  # -min(-x,0) = max(x,0) = reul(x) =
-        D_loss_fake = F.relu(torch.rand_like(fake_data) * 0.2 + 0.8 + D_scores_fake).mean()
+        D_loss_real = F.relu(torch.rand_like(D_scores_real) * 0.2 + 0.8 - D_scores_real).mean()  # -min(-x,0) = max(x,0) = reul(x) =
+        D_loss_fake = F.relu(torch.rand_like(D_scores_fake) * 0.2 + 0.8 + D_scores_fake).mean()
         Dloss = D_loss_real + D_loss_fake
         return Dloss, {"D_scores_real": D_scores_real.mean().item(), "D_scores_fake": D_scores_fake.mean().item()}
 
