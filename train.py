bc4462acacee4d5223a1d794029037b1ba620664
ariel <ariel415el@gmail.com>
2023-04-06 20:36:08 +0300

    Add PLT logger as default

--- train.py
+++ train.py
@@ -17,15 +17,16 @@ from models import get_models
 from utils.common import copy_G_params, load_params
 from losses import get_loss_function, calc_gradient_penalty
 from utils.data import get_dataloader
-from utils.logger import get_dir
+from utils.logger import get_dir, PLTLogger, WandbLogger
+
 
 def get_models_and_optimizers(args):
     netG, netD = get_models(args, device)
     netG.train()
     netD.train()
 
-    optimizerG = optim.Adam(netG.parameters(), lr=args.lrG, betas=(0.5, 0.999))
-    optimizerD = optim.Adam(netD.parameters(), lr=args.lrD, betas=(0.5, 0.999))
+    optimizerG = optim.Adam(netG.parameters(), lr=args.lrG, betas=(0.5, 0.9))
+    optimizerD = optim.Adam(netD.parameters(), lr=args.lrD, betas=(0.5, 0.9))
 
     start_iteration = 0
     if args.resume_last_ckpt:
@@ -43,7 +44,7 @@ def get_models_and_optimizers(args):
 
 
 def train_GAN(args):
-    wandb.init(project=args.wandb_project, dir=plots_image_folder, name=args.name)
+    logger = (WandbLogger if args.wandb else PLTLogger)(args, plots_image_folder)
     debug_fixed_noise = torch.randn((args.batch_size, args.z_dim)).to(device)
     debug_fixed_reals = next(train_loader).to(device)
     debug_fixed_reals_test = next(test_loader).to(device)
@@ -82,12 +83,14 @@ def train_GAN(args):
         # #####  1. train Discriminator #####
         if iteration % args.D_step_every == 0:
             Dloss, debug_Dlosses = loss_function.trainD(netD, real_images, fake_images)
+            if args.gp_weight > 0:
+                gp = args.gp_weight * calc_gradient_penalty(netD, real_images, fake_images)
+                debug_Dlosses['gp'] = gp.item()
+                Dloss += gp
             netD.zero_grad()
             Dloss.backward()
-            if args.gp_weight > 0:
-                Dloss += args.gp_weight * calc_gradient_penalty(netD, real_images, fake_images)
             optimizerD.step()
-            wandb.log(debug_Dlosses, step=iteration)
+            logger.log(debug_Dlosses, step=iteration)
 
         if not args.no_fake_resample:
             noise = torch.randn((b, args.z_dim)).to(device)
@@ -100,7 +103,7 @@ def train_GAN(args):
             netG.zero_grad()
             Gloss.backward()
             optimizerG.step()
-            wandb.log(debug_Glosses, step=iteration)
+            logger.log(debug_Glosses, step=iteration)
 
         # Update avg weights
         for p, avg_p in zip(netG.parameters(), avg_param_G):
@@ -109,13 +112,15 @@ def train_GAN(args):
         if iteration % 100 == 0:
             it_sec = max(1, iteration - start_iteration) / (time() - start)
             print(f"Iteration: {iteration}: it/sec: {it_sec:.1f}")
+            logger.plot()
+
 
         if iteration % args.log_freq == 0:
             backup_para = copy_G_params(netG)
             load_params(netG, avg_param_G)
 
             evaluate(netG, netD, inception_metrics, other_metrics, debug_fixed_noise,
-                     debug_fixed_reals, debug_fixed_reals_test, saved_image_folder, iteration, args)
+                     debug_fixed_reals, debug_fixed_reals_test, saved_image_folder, iteration, logger, args)
             fname = f"{saved_model_folder}/{'last' if not args.save_every else iteration}.pth"
             torch.save({"iteration": iteration, 'netG': netG.state_dict(), 'netD': netD.state_dict(),
                         "optimizerG":optimizerG.state_dict(), "optimizerD": optimizerD.state_dict()},
@@ -125,7 +130,7 @@ def train_GAN(args):
 
 
 def evaluate(netG, netD, inception_metrics, other_metrics, fixed_noise, debug_fixed_reals, debug_fixed_reals_test,
-             saved_image_folder, iteration, args):
+             saved_image_folder, iteration, logger, args):
     netG.eval()
     netD.eval()
     start = time()
@@ -134,26 +139,26 @@ def evaluate(netG, netD, inception_metrics, other_metrics, fixed_noise, debug_fi
         D_noise = netD(fixed_noise_fake_images)
         D_train = netD(debug_fixed_reals)
         D_test = netD(debug_fixed_reals_test)
-        wandb.log({'D_train': D_train.mean().item(),
+        logger.log({'D_train': D_train.mean().item(),
                    'D_test':D_test.mean().item(),
                    'D_noise': D_noise.mean().item(),
                    'rv': (D_train.mean().item() - D_test.mean().item()) / (D_train.mean().item() - D_noise.mean().item()),
                    'rt': (D_train.sign().mean().item()),
-                   'train/test': (D_train.mean().item() / D_test.mean().item()),
-                   'train/fake': (D_train.mean().item() / D_noise.mean().item())
+                   'train\\test': (D_train.mean().item() / D_test.mean().item()),
+                   'train\\fake': (D_train.mean().item() / D_noise.mean().item())
                    }, step=iteration)
 
         if args.ensemble_models != 1:
-            wandb.log({'Ensemble_STD_real_train': netD(debug_fixed_reals, return_std=True)[1],
+            logger.log({'Ensemble_STD_real_train': netD(debug_fixed_reals, return_std=True)[1],
                        'Ensemble_STD_fixed_noise': netD(fixed_noise_fake_images, return_std=True)[1],
                        }, step=iteration)
         
         if args.fid_n_batches > 0 and iteration % args.fid_freq == 0:
             fake_batches = [netG(torch.randn_like(fixed_noise).to(device)) for _ in range(args.fid_n_batches)]
-            wandb.log(inception_metrics(fake_batches), step=iteration)
+            logger.log(inception_metrics(fake_batches), step=iteration)
 
         for metric in other_metrics:
-            wandb.log({
+            logger.log({
                 f'{metric.name}_fixed_noise_gen_to_train': metric(fixed_noise_fake_images, debug_fixed_reals),
                 f'{metric.name}_fixed_noise_gen_to_test': metric(fixed_noise_fake_images, debug_fixed_reals_test),
             }, step=iteration)
@@ -200,6 +205,7 @@ if __name__ == "__main__":
     parser.add_argument('--no_fake_resample', default=False, action='store_true')
 
     # Evaluation
+    parser.add_argument('--wandb', action='store_true', default=False, help="Otherwise use PLT localy")
     parser.add_argument('--log_freq', default=1000, type=int)
     parser.add_argument('--save_every', action='store_true', default=False)
     parser.add_argument('--fid_freq', default=10000, type=int)
@@ -207,8 +213,8 @@ if __name__ == "__main__":
                                                                      "reference FID statistics (0 turns off FID)")
 
     # Other
+    parser.add_argument('--project_name', default='GANs')
     parser.add_argument('--tag', default='test')
-    parser.add_argument('--wandb_project', default='GANs')
     parser.add_argument('--n_workers', default=4, type=int)
     parser.add_argument('--resume_last_ckpt', action='store_true', default=False,
                         help="Search for the latest ckpt in the same folder to resume training")
@@ -216,6 +222,7 @@ if __name__ == "__main__":
     parser.add_argument('--device', default="cuda:0")
 
     args = parser.parse_args()
+
     args.name = f"{os.path.basename(args.data_path)}_{args.im_size}x{args.im_size}_G-{args.gen_arch}" \
                 f"_D-{args.disc_arch}_L-{args.loss_function}_Z-{args.z_dim}_B-{args.batch_size}_{args.tag}"
 
--- utils/logger.py
+++ utils/logger.py
@@ -11,61 +11,42 @@ import matplotlib as mpl
 import wandb
 
 mpl.use('Agg')
-COLORS=['r','g','b','k','pink', 'yellow']
-
 
 class PLTLogger:
-    def __init__(self, save_dir):
+    def __init__(self, args, save_dir):
         self.save_dir = save_dir
         os.makedirs(save_dir, exist_ok=True)
-        self.aggregated_data = defaultdict(list)
+        self.data_avgs = defaultdict(list)
         self.data = defaultdict(list)
-        self.group_names = dict()
-        self.log_file = f"{self.save_dir}/log.pkl"
-        if os.path.exists(self.log_file):
-            self.data = pickle.load(open(self.log_file, "rb"))
 
-    def aggregate_data(self, data_dict, group_name):
-        if group_name not in self.group_names:
-            self.group_names[group_name] = list(data_dict.keys())
+    def log(self, data_dict, step):
         for k, v in data_dict.items():
-            self.aggregated_data[k].append(v)
-
-    def add_data(self, data_dict, group_name):
-        if group_name not in self.group_names:
-            self.group_names[group_name] = list(data_dict.keys())
-        for k, v in data_dict.items():
-            self.data[k].append(v)
+            self.data[k].append(float(v))
 
     def plot(self):
-        # Add averaged aggregated data
-        for k, v in self.aggregated_data.items():
-            self.data[k].append(np.mean(v))
-            self.aggregated_data[k] = []
-
-        # Plot grouped data
-        all_names = []
-        for title, names in self.group_names.items():
-            for i, name in enumerate(names):
-                plt.plot(np.arange(len(self.data[name])), self.data[name], label=name, color=COLORS[i])
-                all_names.append(name)
-            plt.legend()
-            plt.savefig(self.save_dir + f"/{title}.png")
-            plt.clf()
-
         # Plot single plots
         for k, v in self.data.items():
-            if k not in all_names:
-                plt.plot(np.arange(len(self.data[k])), self.data[k])
+            if self.data[k]:
+                self.data_avgs[k] += [np.mean(self.data[k])]
+                self.data[k] = []
+                plt.plot(np.arange(len(self.data_avgs[k])), self.data_avgs[k])
                 plt.title(k)
                 plt.savefig(self.save_dir + f"/{k}.png")
                 plt.clf()
 
-        pickle.dump(self.data, open(self.log_file, "wb"))
 
+class WandbLogger:
+    def __init__(self, args, save_dir):
+        self.wandb = wandb.init(project=args.projec_name, dir=save_dir, name=args.name)
+
+    def log(self, val_dict, step):
+        self.wandb.log(val_dict, step=step)
+
+    def plot(self):
+        pass
 
 def get_dir(args):
-    task_name = os.path.join(f"outputs/{args.wandb_project}",  args.name)
+    task_name = os.path.join(f"outputs", args.project_name,   args.name)
     saved_model_folder = os.path.join(task_name, 'models')
     saved_image_folder = os.path.join(task_name, 'images')
     plots_image_folder = os.path.join(task_name, 'plots')
