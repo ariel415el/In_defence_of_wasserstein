fixed bug of normalizing image; logger shows last value not avg

utils/common.py
@@ -27,9 +27,6 @@ def parse_classnames_and_kwargs(string, kwargs=None):
         kwargs[name] = value
     return class_name, kwargs
 
+
 def dump_images(batch, fname, nrow):
-    batch += 1
-    batch = batch / 2
-    # batch *= 255
-    # batch = torch.clip(batch, 0, 255)
-    save_image(batch, fname, nrow=nrow, normalize=False)
+    save_image((batch + 1)/2, fname, nrow=nrow, normalize=False)

utils/logger.py
@@ -27,10 +27,11 @@ class PLTLogger:
         # Plot single plots
         for k, v in self.data.items():
             if self.data[k]:
+                last_value = self.data[k][-1]
                 self.data_avgs[k] += [np.mean(self.data[k])]
                 self.data[k] = []
                 plt.plot(np.arange(len(self.data_avgs[k])), self.data_avgs[k])
-                plt.title(k + f" Last: {self.data_avgs[k][-1]:.5f}")
+                plt.title(k + f" Last value: {last_value:.5f}")
                 plt.savefig(self.save_dir + f"/{k}.png")
                 plt.clf()
 