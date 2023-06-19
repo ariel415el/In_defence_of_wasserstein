Fixed FC spectral normalization

models/model_utils/spectral_normalization.py
@@ -87,7 +87,7 @@ def set_layer(model, name, layer):
 def make_model_spectral_normalized(model):
     for name, module in model.named_modules():
         if isinstance(module, torch.nn.Linear):
-            new_conv = SNLinear(in_features=module.in_features, out_features=module.out_features, bias=module.bias)
+            new_conv = SNLinear(in_features=module.in_features, out_features=module.out_features, bias=module.bias is not None)
         elif isinstance(module, torch.nn.Conv2d):
             new_conv = SNConv2d(in_channels=module.in_channels,
                                 out_channels=module.out_channels,

scripts/EMD/dists.py
@@ -11,14 +11,14 @@ sys.path.append(os.path.join(os.path.dirname(__file__), "..", ".."))
 from losses.loss_utils import get_dist_metric
 
 
-def emd(x,y, dist='L2'):
+def emd(x,y, dist='L2', numItermax=100000):
     uniform_x = np.ones(len(x)) / len(x)
     uniform_y = np.ones(len(y)) / len(y)
     # M = ot.dist(x, y) / x.shape[1]
     # M = efficient_L2_distances(x, y).cpu().numpy()
     dist_function = get_dist_metric(dist)
     M = batch_dist_matrix(x, y, b=512, dist_function=dist_function).cpu().numpy()
-    dist = ot.emd2(uniform_x, uniform_y, M)
+    dist = ot.emd2(uniform_x, uniform_y, M, numItermax=numItermax)
     # dist = ot.sinkhorn2(uniform_x, uniform_y, M, 1)
     return dist
 