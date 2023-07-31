c044d7712ff66032a025efdc4db71c5ba315c80c
ariel <ariel415el@gmail.com>
2023-07-31 15:18:18 +0300

    Fixed another bug in batch losses

--- losses/optimal_transport.py
+++ losses/optimal_transport.py
@@ -23,7 +23,7 @@ def to_patches(x, p=8, s=4, sample_patches=None):
     patches = patches.permute(0, 2, 1)
     patches = patches.reshape(-1, x.shape[1], p, p)
     if sample_patches is not None:
-        patches = patches[torch.randperm(len(patches))[:sample_patches]]
+        patches = patches[torch.randperm(len(patches))[:int(sample_patches)]]
     return patches
 
 
@@ -62,25 +62,32 @@ def sinkhorn(x, y, epsilon=1, **kwargs):
 
 class MiniBatchLoss:
     def __init__(self, dist='w1', **kwargs):
-        # self.metric = {"w1": w1, "sinkhorn": sinkhorn, "swd": swd}[name]
         self.metric =globals()[dist]
         self.kwargs = kwargs
 
+    def compute(self, x, y):
+        return self.metric(x, y, **self.kwargs)
+
     def __call__(self, images_X, images_Y):
         with torch.no_grad():
-            return self.metric(images_X, images_Y, **self.kwargs)[0]
+            return self.compute(images_X, images_Y)[0]
 
     def trainD(self, netD, real_data, fake_data):
         raise NotImplemented("MiniBatchLosses should be run with --n_D_steps 0")
 
     def trainG(self, netD, real_data, fake_data):
-        return self.metric(real_data, fake_data, **self.kwargs)
+        return self.compute(real_data, fake_data)
 
 
 class MiniBatchPatchLoss(MiniBatchLoss):
-    def __init__(self, dist='w1', p=5, s=1, sample_patches=None, **kwargs):
+    def __init__(self, dist='w1', p=5, s=1, n_samples=None, **kwargs):
         super(MiniBatchPatchLoss, self).__init__(dist,  **kwargs)
-        p = int(p)
-        s = int(s)
-        self.metric = lambda x, y : self.metric(to_patches(x, p, s, sample_patches),
-                                                to_patches(y, p, s, sample_patches))
+        self.p = int(p)
+        self.s = int(s)
+        self.n_samples = n_samples
+
+    def compute(self, x, y):
+        return self.metric(to_patches(x, self.p, self.s, self.n_samples),
+                           to_patches(y, self.p, self.s, self.n_samples), **self.kwargs)
+
+
