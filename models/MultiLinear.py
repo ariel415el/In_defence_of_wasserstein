abb939ea376a27b59654e2f27b662e2c58f0d277
ariel <ariel415el@gmail.com>
2023-08-06 18:14:36 +0300

    Update Multilinear and model ensemble

--- models/MultiLinear.py
+++ models/MultiLinear.py
@@ -3,6 +3,8 @@ import random
 import torch
 from torch import nn
 
+from utils.common import hash_vectors
+
 
 # class Generator(nn.Module):
 #     def __init__(self, z_dim, output_dim=64, channels=3, n_linears=16):
@@ -21,14 +23,16 @@ from torch import nn
 class Generator(nn.Module):
     def __init__(self, z_dim, output_dim=64, channels=3, n_linears=16):
         super(Generator, self).__init__()
-        self.model = nn.Linear(z_dim, channels * output_dim ** 2 * n_linears)
-        self.channels = channels
-        self.output_dim = output_dim
-        self.n_linears = n_linears
+        self.n_linears = int(n_linears)
+        self.channels = int(channels)
+        self.output_dim = (output_dim)
+        self.model = nn.Linear(z_dim, self.channels * self.output_dim ** 2 * self.n_linears)
 
     def forward(self, z):
-        c = torch.randint(0, self.n_linears-1, (z.shape[0], ), dtype=torch.long)
         output = self.model(z).reshape(-1, self.n_linears, self.channels, self.output_dim, self.output_dim)
+        # c = torch.randint(0, self.n_linears-1, (z.shape[0], ), dtype=torch.long)
+        c = hash_vectors(z, n=self.n_linears)
+
         return output[torch.arange(len(c)), c]
 
 
