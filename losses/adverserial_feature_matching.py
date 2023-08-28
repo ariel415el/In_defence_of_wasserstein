e84e5d7b6365b4ad30fff7092c64de615e644e29
ariel <ariel415el@gmail.com>
2023-08-28 09:49:36 +0300

    Upodae Adverserial features loss

--- losses/adverserial_feature_matching.py
+++ losses/adverserial_feature_matching.py
@@ -26,3 +26,26 @@ class AdverserialFeatureMatchingLoss:
         OT = torch.sum(OTPlan * C)
 
         return OT, {"OT": OT.item()}
+
+class FullAdverserialFeatureMatchingLoss:
+    def trainD(self, netD, real_data, fake_data):
+        Dloss = -1* self.compute_ot(netD, real_data, fake_data)
+        return Dloss, {"Dloss": Dloss.item()}
+
+    def trainG(self, netD, real_data, fake_data):
+        OT = self.compute_ot(netD, real_data, fake_data)
+        return OT, {"OT": OT.item()}
+
+    def compute_ot(self, netD, real_data, fake_data):
+        # OT in features space
+        real_features = netD.features(real_data).reshape(len(real_data), -1)
+        fake_features = netD.features(fake_data).reshape(len(fake_data), -1)
+
+        C = torch.mean((real_features[:, None] - fake_features[None, :]) ** 2, dim=-1)
+
+        OTPlan = get_ot_plan(C.detach().cpu().numpy())
+        OTPlan = torch.from_numpy(OTPlan).to(C.device)
+
+        OT = torch.sum(OTPlan * C)
+
+        return OT
